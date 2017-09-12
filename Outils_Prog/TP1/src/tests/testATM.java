c76679a3f707d0273465062380025da50acd7040
bchanez <bastien.chanez@gmail.com>
2017-09-12 10:01:48 +0200

    TP1 Outils_Prog à finir

---
+++ Outils_Prog/TP1/src/tests/testATM.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import fr.ufc.l3info.oprog.*;
+
+public class testATM {
+
+}
