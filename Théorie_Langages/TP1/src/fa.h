3f8e36d3e30d9962e4f2eb900c3adc52fbe3d314
bchanez <bastien.chanez@gmail.com>
2017-09-14 14:29:34 +0200

    ajout des commentaires

--- Théorie_Langages/TP1/src/fa.h
+++ Théorie_Langages/TP1/src/fa.h
@@ -1,13 +1,20 @@
+// crée un automate
 void fa_create(struct fa *self, size_t alpha_count, size_t state_count);
 
+// détruire un automate
 void fa_destroy(struct fa *self);
 
+// rentre un état initial
 void fa_set_state_initial(struct fa *self, size_t state);
 
+// rendre un état final
 void fa_set_state_final(struct fa *self, size_t state);
 
+// ajouter un transition à l'automate
 void fa_add_transition(struct fa *self, size_t from, char alpha, size_t to);
 
+// afficher un automate
 void fa_pretty_print(const struct fa *self, FILE *out);
 
+// affiche en automate en format DOT
 void fa_dot_print(const struct fa *self, FILE *out);
