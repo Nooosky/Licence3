22803c370cf3a70c1b3b82e6e4c72e1c9ab2b07e
Jérémy Roussey <jeremyroussey@yahoo.fr>
2017-09-14 14:25:43 +0200

    First question done --Need testing--

--- Théorie_Langages/TP1/src/fa.c
+++ Théorie_Langages/TP1/src/fa.c
@@ -1,6 +1,11 @@
-void fa_create(struct fa *self, size_t alpha_count, size_t state_count)
-{
+void fa_create(struct fa *self, size_t alpha_count, size_t state_count) {
+    self->alpha_count = alpha_count;
+    self->state_count = state_count;
 
+    for (int i = 0; i < state_count; ++i) {
+        struct state new_state = (struct state) {.is_initial = 0, .is_final =0};
+        self->states = &new_state;
+    }
 }
 
 void fa_destroy(struct fa *self)
