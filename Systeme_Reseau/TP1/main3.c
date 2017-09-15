a033c2fc7c86603c69c2277cfcb459e79b9bccb1
bchanez <bastien.chanez@gmail.com>
2017-09-15 14:38:32 +0200

    correction tp1 exercice 3 (probleme de concatenation) + ajout de dossier pour les tp de securite

--- Systeme_Reseau/TP1/main3.c
+++ Systeme_Reseau/TP1/main3.c
@@ -9,7 +9,9 @@
 
 int main (int argc, char *argv[]) 
 {
-    if (argc >= 2)
+
+    
+    if (argc > 2)
     {
         fprintf(stderr, "USAGE: ./main3 [<shmID>]\n");
         exit(1);
@@ -23,94 +25,87 @@ int main (int argc, char *argv[])
     int shmID; // id du segment mémoir partagée
     char *p; // pointeur sur l'adresse du segment de mémoire partagée
 
-    switch (argc) 
+    printf("%d\n", argc); 
+    if (argc == 2) // 1 argument 
     {
-        case 1 : // 1 argument 
-            printf("processus 1");
+        printf("processus 1 \n");
 
 
-            shmID = atoi(argv[1]);
-            // affichage du shmID du semgent
-            printf("shmid segment : %i\n",shmID);
+        shmID = atoi(argv[1]);
+        // affichage du shmID du semgent
+        printf("shmid segment : %i\n",shmID);
 
-            // attache le segment de mémoire partagée
-            if ((p = (char *) shmat(shmID, NULL, 0)) == (void*) -1)
-            {
-                perror("shmat");
-                exit(1);
-            }
-            
-            // temporisation clavier
-            printf("temporisation : recevoir message \n");
-            getchar();
+        // attache le segment de mémoire partagée
+        if ((p = (char *) shmat(shmID, NULL, 0)) == (void*) -1)
+        {
+            perror("shmat");
+            exit(1);
+        }
 
-            // on affiche le message
-            printf("%s\n", p);
+        // on affiche le message
+        printf("%s\n", p);
 
-            // donne une valeur
-            message = "..John";
-            strcat(p, message);
+        // donne une valeur
+        message = "Bonjour ";
+        strcat(p, message);
 
-            // détache le segment mémoire
-            if (shmdt(p) < 0)
-            {
-                perror("shmdt");
-                exit(1);
-            }
+        // détache le segment mémoire
+        if (shmdt(p) < 0)
+        {
+            perror("shmdt");
+            exit(1);
+        }
+    }
+    else // 0 argument
+    {
+        printf("processus 2 \n");
+                
+        // définition de la clé
+        // key = 1234;
+        key = ftok("./", 'h');
 
-        break;
+        // création du segment mémoire partagée avec les droits rw r r
+        if ((shmID = shmget(key, sizeof(message), IPC_CREAT | 0644)) < 0)
+        {
+            perror("shmget");
+            exit (1);
+        }
+    
+        // affichage du shmID du semgent crée
+        printf("shmid segment : %i\n",shmID);
 
-        default : // 0 argument
-            printf("processus 2");
-                    
-            // définition de la clé
-            // key = 1234;
-            key = ftok("./", 'h');
 
-            // création du segment mémoire partagée avec les droits rw r r
-            if ((shmID = shmget(key, sizeof(message), IPC_CREAT | 0644)) < 0)
-            {
-                perror("shmget");
-                exit (1);
-            }
+        // attache le segment de mémoire partagée
+        if ((p = (char *) shmat(shmID, NULL, 0)) == (void*) -1)
+        {
+            perror("shmat");
+            exit(1);
+        }
+
+        // donne une valeur
+        message = "John";
+        strcat(p, message);
+
+        // temporisation clavier
+        printf("temporisation : recevoir message  \n");
+        getchar();
         
-            // affichage du shmID du semgent crée
-            printf("shmid segment : %i\n",shmID);
+        // on affiche le message
+        printf("%s\n", p);
 
-
-            // attache le segment de mémoire partagée
-            if ((p = (char *) shmat(shmID, NULL, 0)) == (void*) -1)
-            {
-                perror("shmat");
-                exit(1);
-            }
-
-            // donne une valeur
-            message = "John";
-            strcat(p, message);
-
-            // temporisation clavier
-            printf("temporisation : recevoir message  \n");
-            getchar();
-            
-            // on affiche le message
-            printf("%s\n", p);
-
-            // détache le segment mémoire
-            if (shmdt(p) < 0)
-            {
-                perror("shmdt");
-                exit(1);
-            }
-            
-            // supprime le segment de mémoire partagée
-            if (shmctl(shmID,IPC_RMID,NULL) == -1)
-            {
-                perror("shmctl") ;
-                exit(1) ;
-            }
-
-        break;
+        // détache le segment mémoire
+        if (shmdt(p) < 0)
+        {
+            perror("shmdt");
+            exit(1);
+        }
+        
+        // supprime le segment de mémoire partagée
+        if (shmctl(shmID,IPC_RMID,NULL) == -1)
+        {
+            perror("shmctl") ;
+            exit(1) ;
+        }
     }
 
     return 0;
