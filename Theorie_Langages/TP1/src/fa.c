2f8b24c92a1570fbdcbc02fc1d7d0596da593834
bchanez <bastien.chanez@gmail.com>
2017-09-20 16:28:43 +0200

    TP1 theorie langages correction (encore des problemes)

--- Theorie_Langages/TP1/src/fa.c
+++ Theorie_Langages/TP1/src/fa.c
@@ -1,31 +1,50 @@
 #include "fa.h"
 
 // crée un automate
-void fa_create(struct fa *self, size_t alpha_count, size_t state_count) {
+void fa_create(struct fa *self, size_t alpha_count, size_t state_count)
+{
     self->alpha_count = alpha_count;
     self->state_count = state_count;
 
-    for (int i = 0; i < state_count; ++i) {
-        struct state new_state = (struct state) malloc(sizeof(struct state));
-        
-        new_state = {.is_initial = 0, .is_final =0};
-        self->states = &new_state;
+    self->states = (struct state *) malloc(sizeof(struct state) * self->state_count);
+
+    for (int i = 0; i < self->state_count; ++i)
+    {
+        struct state * new_state = (struct state *) malloc(sizeof(struct state));
+
+        new_state->is_initial = 0;
+        new_state->is_final = 0;
+
+        self->states[i] = &new_state;
     }
 }
 
 // détruire un automate
 void fa_destroy(struct fa *self)
 {
+  for (int i = 0; i < self->state_count; ++i)
+    free(self->states[i]);
 
+  free(self->states);
+  free(self);
 }
+
 //make a state initial
-void fa_set_state_initial(struct fa *selstrcutf, size_t state) {
-    self->states[state].is_initial = 1;
+void fa_set_state_initial(struct fa *self, size_t state)
+{
+    if(-1 > state && state < self->state_count)
+      self->states[state].is_initial = 1;
+    else
+      perror("fa_set_state_initial");
 }
 
 //make a state final
-void fa_set_state_final(struct fa *self, size_t state) {
-    self->states[state].is_final = 1;
+void fa_set_state_final(struct fa *self, size_t state)
+{
+    if(-1 > state && state < self->state_count)
+      self->states[state].is_final = 1;
+    else
+      perror("fa_set_state_final");
 }
 
 // ajouter un transition à l'automate
@@ -43,5 +62,5 @@ void fa_pretty_print(const struct fa *self, FILE *out)
 // affiche en automate en format DOT
 void fa_dot_print(const struct fa *self, FILE *out)
 {
-    
+
 }
--- Theorie_Langages/TP1/src/testfa.c
+++ Theorie_Langages/TP1/src/testfa.c
@@ -1,10 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "fa.h"
 
-
 int main() {
+
+    struct fa *self = (struct fa *) malloc(sizeof(struct fa));
+
     
     //Creation of a automaton
-    struct fa *self = (struct fa *) malloc(sizeof(struct fa));
     fa_create(&self, 3, 3);
 
     //destruction of a automaton
