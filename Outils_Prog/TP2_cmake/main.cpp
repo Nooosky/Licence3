150bd34dca186f3073e84cd44dc0b55676f7a6da
bchanez <bastien.chanez@gmail.com>
2017-09-26 11:10:47 +0200

    cMakeLists du tp2 de outil prog

--- Outils_Prog/TP2_cmake/main.cpp
+++ Outils_Prog/TP2_cmake/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+#ifdef _SYSTEM_LINUX
+  #include <stdio.h>
+#elif _SYSTEM_WINDOWS
+  #include <windows.h>
+#endif
+
+#include "lib/DiplayMessage.h"
+
+
 int main (void)
 {
 
-  printf("Hello world\n");
+  DiplayMessage("Hello world\n");
 
   return 0;
 }
