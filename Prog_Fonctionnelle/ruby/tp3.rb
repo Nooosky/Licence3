3ee6228c5baafe1688a73e82a23d7908c8570555
Jeremy <jeremyroussey@yahoo.fr>
2017-11-23 08:45:45 +0100

    Erreur regexp corrigee

--- Prog_Fonctionnelle/ruby/tp3.rb
+++ Prog_Fonctionnelle/ruby/tp3.rb
@@ -27,13 +27,13 @@ a.sort do |x, y| - (x <=> y) end
 
 def sort_both_fixnum_string(a0)
   a0.sort do |x0, x1|
-    if x0.class == Fixnum or x0 =~ /\A\s*(?:\.[0-9]+)?\s*\z/
-      if x1.class == Fixnum or x1 =~ /\A\s*(?:\.[0-9]+)?\s*\z/
+    if x0.class == Fixnum or x0 =~ /\A\s*(?:\+|-)?[0-9]+(?:\.[0-9]+)?\s*\z/
+      if x1.class == Fixnum or x1 =~ /\A\s*(?:\+|-)?[0-9]+(?:\.[0-9]+)?\s*\z/
         x0.to_i <=> x1.to_i
       else
         -1
       end
-    elsif x1.class == Fixnum or x1 =~ /\A\s*(?:\.[0-9]+)?\s*\z/
+    elsif x1.class == Fixnum or x1 =~ /\A\s*(?:\+|-)?[0-9]+(?:\.[0-9]+)?\s*\z/
       1
     else
       x0 <=> x1
@@ -44,7 +44,7 @@ end
 def sort_both_fixnum_string_v2(a0)
   touch =
     Proc.new do |x|
-      x.class == Fixnum or x =~ /\A\s*(?:\.[0-9]+)?\s*\z/
+      x.class == Fixnum or x =~ /\A\s*(?:\+|-)?[0-9]+(?:\.[0-9]+)?\s*\z/
     end
   a0.sort do |x0, x1|
     touch_x0 = touch(x0)
