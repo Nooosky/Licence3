6cce1243edfe1df27f17f1e80ee66d99ffe7d8a3
Jeremy <jeremyroussey@yahoo.fr>
2017-11-30 10:29:46 +0100

    tp4 encore

--- Prog_Fonctionnelle/ruby/tp4.rb
+++ Prog_Fonctionnelle/ruby/tp4.rb
@@ -34,17 +34,10 @@ def superscan_v2(path)
   unsaved_a = []
   renamed_a = []
   remaining_a = []
-<<<<<<< HEAD
   exists_or_about =
     Proc.new do |path_0|
       File.exist?(path_0) or renamed_a.find do |path_1| path_1 == path_0 end
       end
-=======
-  exist_or_about =
-    Proc.new do |path_0|
-      File.exists?(path_0) or renamed_a.find do |path_1| path_1 == path_0 end
-    end
->>>>>>> 71afab5c440a581b3263ceb0014d6da7fdead15b
 
   Find.find(path) do |path_0|
     if File.directory?(path_0)
@@ -67,4 +60,29 @@ def superscan_v2(path)
     end
   end
   unsaved_a + remaining_a
+end
+
+
+def scan_and_rename(path)
+  unsaved_a = []
+  Find.find(path) do |path_0|
+    if File.directory?(path_0)
+      if path_0.length > 200
+        unsaved_a.push(path_0)
+      else
+        the_dirname, the_basename = File.split(path_0)
+        unless the_basename.ascii_only? and the_dirname !~ $s_regexp
+          basename_0 = the_basename.removeaccents.gsub($s_regexp, '_')
+          basename_1 = basename_0
+          nb_4_suffix = 1
+          while exists_or_about[path_1 = File.join(the_dirname, basename_1)]
+            basename_1 = "#{basename_0}-#{nb_4_suffix}"
+            nb_4_suffix += 1
+          end
+          File.rename(path_0, path_1)
+        end
+      end
+    end
+  end
+  unsaved_a
 end
