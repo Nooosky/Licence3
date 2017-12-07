597241461ee0afc94893a15eb68feba6e53da879
Jeremy <jeremyroussey@yahoo.fr>
2017-12-07 10:01:55 +0100

    Edited

--- Prog_Fonctionnelle/ruby/hamming.rb
+++ Prog_Fonctionnelle/ruby/hamming.rb
@@ -4,4 +4,16 @@ hamming_number_thread = Thread.new do
   loop
     Thread::current[:current_hamming_number] = current_hamming_number
     sleep
-    
+    multiples_h.each do |key.value_a|
+      ** multiplier par la clef et mettre en queue de chaque tableau **
+    end
+    head_a = []
+    multiples_h.each_value do |value_a|
+      ** recuperer les elements de tete sans les enlever **
+    end
+    current_hamming_number = head_a.min
+    multiples_h.each_value do |value_a|
+      **virer le minimum chaque fois qu'on le trouve en tete **
+    end
+  end
+end
