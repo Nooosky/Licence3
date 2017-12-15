391637828736144b139461e04b4c2ea0dac0a2f4
bchanez <bastien.chanez@gmail.com>
2017-12-15 17:18:58 +0100

    tl

--- Theorie_Langages/TP/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/near/Documents/Licence3/Theorie_Langages/TP
-# Build directory: /home/near/Documents/Licence3/Theorie_Langages/TP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit "/home/near/Documents/Licence3/Theorie_Langages/TP/build/unit_tests")
